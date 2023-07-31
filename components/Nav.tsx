92bd929482834fb22d7e20763617af45e564ffa4
jean00 <jeanlouis433@gmail.com>
2023-07-31 12:04:25 +0200

    first commit and set up folders

---
+++ components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = (): React.JSX.Element => {
+  return (
+    <nav className="w-full flex ">
+      <h1>Movies Viewer</h1>
+    </nav>
+  );
+};
+
+export default Nav;
